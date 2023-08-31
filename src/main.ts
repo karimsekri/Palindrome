e3b353e9549a51d86326732c61e86fc0740f462e
karim <karim.sekri@gmail.com>
2023-08-31 12:31:26 +0200

    Ajout de la boucle WHILE

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,26 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+console.log(isPalindrome("kayakf"));
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+// function isPalindrome(maChaine: string): boolean {
+//   for (let index = 0; index < maChaine.length; index++) {
+//     const elementGauche = maChaine[index];
+//     const elementDroite = maChaine[maChaine.length-1-index] ;
+//     if (elementGauche != elementDroite) {
+//       return false;
+//     }
+    
+//   }  
+//   return true;
+// }
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function isPalindrome(maChaine: string): boolean {
+  const lengthMaChaine = maChaine.length;
+  let index = 0 ;
+  
+  while (index < lengthMaChaine-1-index) {
+    if (maChaine[index] != maChaine[lengthMaChaine-1-index]) {
+      return false
+    }
+    index++;
+  }
+  return true 
+}
